7ffd43455c38728bfbe9985ce98b7e7e1e07a888
WhiteGori <juanmblancog@gmail.com>
2025-10-09 18:41:06 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 // server.js
-const app = require("./app");
+const app = require("./app_nuevo");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server en http://localhost:${PORT}`));
